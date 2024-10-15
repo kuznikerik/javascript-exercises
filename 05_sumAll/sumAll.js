da18db6cc1a555af99337d38c36757e7604e1b8b
kuznikerik <erik.kuznik@gmail.com>
2024-10-15 21:38:35 +0200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  // Check if positive interger
+  if (
+    num1 < 1 ||
+    num2 < 1 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
 
+  const min = num1 < num2 ? num1 : num2;
+  const max = num1 > num2 ? num1 : num2;
+  let sumAll = 0;
+
+  for (let i = min; i <= max; i++) {
+    sumAll += i;
+  }
+
+  return sumAll;
 };
+
+sumAll(100, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
