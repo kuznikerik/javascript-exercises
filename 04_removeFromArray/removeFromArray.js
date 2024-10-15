8fc2fd7b3baf594d71d30f38bbe002a6982f162b
kuznikerik <erik.kuznik@gmail.com>
2024-10-15 20:34:08 +0200

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  if (!Array.isArray(arguments[0])) return;
+  const valuesToRemove = Array.from(arguments).slice(1);
+  return arguments[0].filter((element) => !valuesToRemove.includes(element));
 };
 
 // Do not edit below this line
