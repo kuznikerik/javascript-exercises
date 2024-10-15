04b309470fd395a67a43f05d8b11f21ad79bfe36
kuznikerik <erik.kuznik@gmail.com>
2024-10-15 22:09:19 +0200

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  // x °F ≘ (x − 32) × ⁠5 / 9) °C
+  const tempInC = (tempInF - 32) * (5 / 9);
+  return Math.round(tempInC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+  // x °C ≘ (x × ⁠9 / 5 + 32) °F
+  const tempInF = tempInC * (9 / 5) + 32;
+  return Math.round(tempInF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
