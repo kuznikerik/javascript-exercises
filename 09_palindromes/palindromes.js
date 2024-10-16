44af94626213187eba607f4cb6d7237a54bf33c6
kuznikerik <erik.kuznik@gmail.com>
2024-10-16 17:35:55 +0200

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanString = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const array = Array.from(cleanString);
+  return cleanString === array.reverse().join("");
 };
 
 // Do not edit below this line
