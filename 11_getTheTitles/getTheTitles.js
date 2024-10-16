6cd59feab35c21673279209087f5b897ed1d30ad
kuznikerik <erik.kuznik@gmail.com>
2024-10-16 17:50:12 +0200

    Complete 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (obj) {
+  const arrayOfTitles = Array.from(
+    obj.map((thing) => {
+      return thing.title;
+    })
+  );
 
+  return arrayOfTitles;
 };
 
 // Do not edit below this line
