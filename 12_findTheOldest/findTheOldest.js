d02e8f3520c2d956fddd93bea8fd7bb886860a7b
kuznikerik <erik.kuznik@gmail.com>
2024-10-16 18:32:06 +0200

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
-
+const getAge = (birthYear, deathYear) => {
+  const currentYear = new Date().getFullYear();
+  return (deathYear || currentYear) - birthYear;
 };
+
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const currentPersonAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    const oldestPersonAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath
+    );
+
+    return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+  });
+};
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
